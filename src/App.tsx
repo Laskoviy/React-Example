45336e5e1eb6c1c055b0a1bb3ba4ac334dc9baf2
Laskoviy <mzhidelev@yandex.ru>
2022-08-11 15:05:53 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Product } from './components/product';
+import { IProduct } from './models';
+
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([])
+  const [loading, setLoading] = useState(false) //для индикации загрузки
+
+ async function fetchProducts () {
+  setLoading(true)
+ const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+ setProducts(response.data)
+ setLoading(false)
+ }
+
+useEffect( () =>{
+  fetchProducts()
+}, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<div className="container mx-auto max-w-2xl pt-5">
+{loading && <p className='text-center'>Loading...</p>}
+{ products.map(product => <Product product={product} key = {product.id}/>)}
+
+  {/* <Product product = {products[0]}/>
+  <Product product = {products[1]}/> */}
+</div>
+)
 }
 
 export default App;
